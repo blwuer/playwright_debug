a63cdd06be51483aa26a55e0299f79daf092ba64
yangyu <yangyu@mail.com>
2021-09-16 15:30:09 +0800

    add html

--- main.py
+++ main.py
@@ -11,11 +11,23 @@ async def async_get_page_source(url):
         page.on("response", lambda response: print("<<", response.status, response.url))
 
         await page.goto(url)
+        # await page.content() raise Error : Execution context was destroyed, most likely because of a navigation.
+        # I am guessing because the html contains meta http-equiv="refresh"
         page_source = await page.content()
         await browser.close()
         return page_source
-
+        raise
 if __name__ == "__main__":
+    """
+    <html>
+    <head>
+            <meta http-equiv="refresh" content="0; url=https://www.debian.org/blends/">
+    </head>
+    <body>
+            <h1>Debian Pure Blends</h1>
+    </body>
+    </html>
+    """
     url = "https://playwright.neocities.org/"
     loop = asyncio.get_event_loop()
     ret = loop.run_until_complete(async_get_page_source(url=url))
